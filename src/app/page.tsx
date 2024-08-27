637fb8d41882755222e6868644714ab9c41d0bcb
musjj <72612857+musjj@users.noreply.github.com>
2024-08-27 17:51:34 +0700

    feat: remove prefetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,6 @@ import { ErrorBoundary } from "react-error-boundary";
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
-  void api.post.getLatest.prefetch();
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
